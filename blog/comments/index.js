56ad5fac93269f437465d2c9599b7751a91bb648
Arham Doshi <arhamdoshi@Arhams-MacBook-Air.local>
2022-07-13 08:10:30 +0530

    added comments service

---
+++ blog/comments/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended : false}));
+app.use(bodyParser.json())
+
+
+const getCommentsByPostId = {};
+
+app.get("/posts/:id/comments",(req, res) => {
+    res.send((getCommentsByPostId[req.params.id] || []));
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+    const comments = getCommentsByPostId[req.params.id] || [];
+    const content = req.body.content;
+    const newId = randomBytes(4).toString("hex");
+    comments.push({"id" : newId, content});
+    getCommentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log("listening to port 4001");
+})
