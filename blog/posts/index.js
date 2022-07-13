94058f95019e17c0eef808213f000da689b8a2ba
Arham Doshi <arhamdoshi@Arhams-MacBook-Air.local>
2022-07-13 07:21:52 +0530

    completed post

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -3,22 +3,31 @@ const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const posts = {}
 
 app.get("/posts", (req, res) => {
-    res.send("welcome to post");
+    res.send(posts);
 });
 
 
 app.post("/posts", (req, res) => {
-    // var post = {
-    //     id : randomBytes(4).toString("hex"),
-    //     title : req.body
-    // } ;
     
-    res.send("in the posts");
+        const id = randomBytes(4).toString("hex");
+    const   {title} = req.body;
+    console.log(title);
+    console.log(req.body);
+    console.log(req.url);
+    posts[id] = {
+        id, title
+    }
+    console.log("here");
+    
+    res.send(posts[id]);
+    console.log(posts);
 });
 
 app.listen(4000, () => {
-    console.log("posts server started at port 4004")
+    console.log("posts server started at port 4000")
 });
